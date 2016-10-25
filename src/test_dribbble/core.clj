daebb2e5c39907d5106b1dc1675a35c5c58ad7a1
Max Prokopov <mprokopov@gmail.com>
2016-10-25 19:23:58 +0300

    Results seems to be valid. Cleaned up code

--- src/test_dribbble/core.clj
+++ src/test_dribbble/core.clj
@@ -2,23 +2,6 @@
   (:require [clojure.string :as str]
             [clojure.core.match :refer [match]]))
 
-(defprotocol Service
-  (recognize [proto pattern])
-  (get-params [this]))
-
-;; (defn recognize [pat strn]
-;;   [1 2 3])
-
-(defrecord Pattern [pattern]
-  Service
-  (get-params [pattern] pattern)
-  (recognize [proto pattern]
-             proto))
-
-(def twitter (Pattern. "host(twitter.com); path(?user/status/?id);"))
-
-(recognize twitter "http://twitter.com/bradfitz/status/562360748727611392")
-;; => [[:id 562360748727611392] [:user "bradfitz"]]
 
 ;; (let [[_ _ user id]  (re-matches #"[a-z]+://(twitter.com)/(.+)/status/(.+)"   "http://twitter.com/bradfitz/status/562360748727611392")]
 ;;   [:id id
@@ -26,25 +9,25 @@
 
 
 (defn url->map [url]
-  "returns URL parsed to map"
+  "returns URL parsed to map schema host path and queryparam"
   (let [[_ schema domain path queryparam]  (re-matches #"([a-z]+)://([^/]+)/([^\?]*)[\?]?(.*)"   url)]
     {:schema schema
      :host domain
      :path path
      :queryparam queryparam}))
 
-(url->map "http://twitter.com/bradfitz/status/562360748727611392")
-(url->map "https://dribbble.com/shots/1905065-Travel-Icons-pack?list=users&offset=1")
-(url->map "https://dribbble.com/shots/1905065-Travel-Icons-pack?list=users")
+;; (url->map "http://twitter.com/bradfitz/status/562360748727611392")
+;; (url->map "https://dribbble.com/shots/1905065-Travel-Icons-pack?list=users&offset=1")
+;; (url->map "https://dribbble.com/shots/1905065-Travel-Icons-pack?list=users")
 
 (defn parse-keys [input]
   "returns keys for URL parsing"
   (map str/trim (str/split input #";")))
 
-(def arr-1 (first (parse-keys "host(dribbble.com); path(shots/?id); queryparam(offset=?offset);")))
-(def arr-2 (second (parse-keys "host(dribbble.com); path(shots/?id); queryparam(offset=?offset);")))
-(def arr-3 (nth (parse-keys "host(dribbble.com); path(shots/?id); queryparam(offset=?offset);") 2))
-(def arr-4 (last (parse-keys "host(twitter.com); path(?user/status/?id);"))) ;; "http://twitter.com/bradfitz/status/562360748727611392"
+;; (def arr-1 (first (parse-keys "host(dribbble.com); path(shots/?id); queryparam(offset=?offset);")))
+;; (def arr-2 (second (parse-keys "host(dribbble.com); path(shots/?id); queryparam(offset=?offset);")))
+;; (def arr-3 (nth (parse-keys "host(dribbble.com); path(shots/?id); queryparam(offset=?offset);") 2))
+;; (def arr-4 (last (parse-keys "host(twitter.com); path(?user/status/?id);"))) ;; "http://twitter.com/bradfitz/status/562360748727611392"
 
 ;; (defn get-vals [input-text]
 ;;   "replace ?val to regexp matcher and retrieve keys"
@@ -67,18 +50,15 @@
 
 ;; [:queryparam ["user" "id" "(.*)/status/(.*)"]]
 
-(map second (re-seq #"\?([^/]+)" "?user/status/?id"))
-(re-seq #"\?([^/]+)" "?user/?status/sdfid")
-(re-seq #"\?([^/]+)" "user/status/?sdfid")
-(str/replace "?user/status/?id" #"\?([^/]+)" "(.*)")
+;; (map second (re-seq #"\?([^/]+)" "?user/status/?id"))
+;; (re-seq #"\?([^/]+)" "?user/?status/sdfid")
+;; (re-seq #"\?([^/]+)" "user/status/?sdfid")
+;; (str/replace "?user/status/?id" #"\?([^/]+)" "(.*)")
 
-;; (get-vals arr-2)
-;; (get-vals arr-3)
-;; (get-vals arr-4)
-(retrieve-pattern arr-1)
-(retrieve-pattern arr-2)
-(retrieve-pattern arr-3)
-(retrieve-pattern arr-4)
+;; (retrieve-pattern arr-1)
+;; (retrieve-pattern arr-2)
+;; (retrieve-pattern arr-3)
+;; (retrieve-pattern arr-4)
 
 ;; (re-matches #".*\?(.*)" "list=?type")
 
@@ -90,6 +70,7 @@
 ;;     [(keyword key-name) matched]))
 
 (defn retrieve-values [url pattern]
+  "retrieve values by pattern applying regex transformation or nil if pattern not matches"
   (let [url-map (url->map url) ;; (url->map "https://dribbble.com/shots/1905065-Travel-Icons-pack?list=users&offset=1")
         [k1 _ keys-arr rexp] (retrieve-pattern pattern) ;; (retrieve-pattern arr-1)
         tested-val ((keyword k1) url-map) ;; (re-find (re-pattern "(.*)/status/(.*)") "bradfitz/status/562360748727611392")
@@ -106,10 +87,14 @@
 ;;   (vec found))
 
 
-(retrieve-values "http://twitter.com/bradfitz/status/562360748727611392" arr-4)
-(retrieve-values "https://dribbble.com/shots/1905065-Travel-Icons-pack?list=users&offset=1" arr-1)
-(retrieve-values "https://dribbble.com/shots/1905065-Travel-Icons-pack?list=users&offset=1" arr-2)
-(retrieve-values "https://dribbble.com/shots/1905065-Travel-Icons-pack?list=users&offset=1" arr-3)
+;; (retrieve-values "http://twitter.com/bradfitz/status/562360748727611392" arr-4)
+;; [[:user "bradfitz"] [:id "562360748727611392"]]
+;; (retrieve-values "https://dribbble.com/shots/1905065-Travel-Icons-pack?list=users&offset=1" arr-1)
+;; []
+;; (retrieve-values "https://dribbble.com/shots/1905065-Travel-Icons-pack?list=users&offset=1" arr-2)
+;; [[:id "1905065-Travel-Icons-pack"]]
+;; (retrieve-values "https://dribbble.com/shots/1905065-Travel-Icons-pack?list=users&offset=1" arr-3)
+;; [[:offset "1"]]
 
 ;; (let [url "https://dribbble.com/shots/1905065-Travel-Icons-pack?list=users&offset=1"
 ;;       patterns (parse-keys "host(dribbble.com); path(shots/?id); queryparam(offset=?offset);")
@@ -122,19 +107,22 @@
         is-invalid (some nil? coll)]
     (when-not is-invalid coll)))
 
-(recognize2 "http://twitter.com/mprokopov/status/5748727611392/asdfsdf" "host(twitter.com); path(?user/status/?id/?sdf);")
-(recognize2 "https://dribbble.com/shots/1905065-Travel-Icons-pack?list=users&offset=1" "host(dribbble.com); path(shots/?id); queryparam(offset=?offset);")
+;; (recognize2 "http://twitter.com/mprokopov/status/5748727611392/asdfsdf" "host(twitter.com); path(?user/status/?id/?sdf);")
+;; (recognize2 "https://dribbble.com/shots/1905065-Travel-Icons-pack?list=users&offset=1" "host(dribbble.com); path(shots/?id); queryparam(offset=?offset);")
 
-;; (re-matches #"(.*)/status/(.*)" "/bradfitz/status/562360748727611392")
+;; TODO: -> transform value to int
+;; TODO: cleanup output array for empty values
+(defprotocol Service
+  (recognize [pattern url]))
 
+(defrecord Pattern [pattern]
+  Service
+  (recognize [pattern url]
+             (recognize2 url (:pattern pattern))))
 
-;; (def pat (re-pattern "http[s]?://dribbble.com/shots/(.*)\\?list=users&offset=(.+)"))
-;; (re-matches pat "https://dribbble.com/shots/1905065-Travel-Icons-pack?list=users&offset=1")
-
-;; (let [host "dribbble.com"
-;;       path "shots/"
-;;       pat (str "http[s]?://" host "/" path "(.*)\\?list=users&offset=(.+)")]
-;;   (re-matches (re-pattern pat) "https://dribbble.com/shots/1905065-Travel-Icons-pack?list=users&offset=1"))
+(def twitter (Pattern. "host(twitter.com); path(?user/status/?id);"))
+(recognize twitter "http://twitter.com/bradfitz/status/562360748727611392")
+;; => [[:id 562360748727611392] [:user "bradfitz"]]
 
 (def dribbble (Pattern. "host(dribbble.com); path(shots/?id); queryparam(offset=?offset);"))
 (recognize dribbble "https://dribbble.com/shots/1905065-Travel-Icons-pack?list=users&offset=1")
@@ -145,3 +133,4 @@
 ;; => nil ;; offset queryparam missing
 
 (def dribbble2 (Pattern. "host(dribbble.com); path(shots/?id); queryparam(offset=?offset); queryparam(list=?type);"))
+
